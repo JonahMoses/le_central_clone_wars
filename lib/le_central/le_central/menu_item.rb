13d6c2e903c01bf1e90ca69e6749d1861e6942a6
Jonah <Jonahkmoses@gmail.com>
2013-10-30 10:50:48 -0600

    Added admin page layout

--- lib/le_central/le_central/menu_item.rb
+++ lib/le_central/le_central/menu_item.rb
@@ -8,16 +8,16 @@ module LeCentral
     end
 
     def build_item
-      @id = menu_item_hash[:id]
-      @active = menu_item_hash[:active]
-      @meal = menu_item_hash[:meal]
-      @course = menu_item_hash[:course]
-      @menu_order = menu_item_hash[:menu_order]
-      @name = menu_item_hash[:name]
+      @id          = menu_item_hash[:id]
+      @active      = menu_item_hash[:active]
+      @meal        = menu_item_hash[:meal]
+      @course      = menu_item_hash[:course]
+      @menu_order  = menu_item_hash[:menu_order]
+      @name        = menu_item_hash[:name]
       @description = menu_item_hash[:description]
-      @price = menu_item_hash[:price]
-      @created_at = menu_item_hash[:created_at]
-      @updated_at = menu_item_hash[:updated_at]
+      @price       = menu_item_hash[:price]
+      @created_at  = menu_item_hash[:created_at]
+      @updated_at  = menu_item_hash[:updated_at]
     end
 
   end
